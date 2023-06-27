10f86359a2fe9251f3d618fcb1e80a428495f1b5
Shrinath <shrinathsgavadoji@gmail.com>
2023-06-27 11:32:33 +0530

    commit

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
+
 import '../model/heros_model.dart';
 
 class DetailScreen extends StatefulWidget {
@@ -13,6 +16,8 @@ class DetailScreen extends StatefulWidget {
 
 class _DetailScreenState extends State<DetailScreen> {
   bool animate = false;
+
+  bool reverseScroll = false;
   @override
   void initState() {
     Future.delayed(const Duration(milliseconds: 200), () {
@@ -34,151 +39,267 @@ class _DetailScreenState extends State<DetailScreen> {
       backgroundColor: widget.color,
       body: Stack(
         children: [
-          Positioned.fill(
-            child: Stack(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: deviceSize.height * 0.4,
-                    ),
-                    Text(
-                      widget.hero.heroName,
+          // Positioned.fill(
+          // child:
+          Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: deviceSize.height * 0.4,
+                  ),
+                  Text(
+                    widget.hero.heroName,
+                    style: const TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Text(
+                      widget.hero.heroInfo,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 7,
                       style: const TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
                           color: Colors.white),
                     ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                      child: Text(
-                        widget.hero.heroInfo,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 7,
-                        style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.white),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                  ],
-                ),
-                AnimatedPositioned(
-                  duration: const Duration(milliseconds: 400),
-                  top: animate ? 0 : -300,
-                  curve: Curves.fastEaseInToSlowEaseOut,
-                  child: SizedBox(
-                    width: deviceSize.width,
-                    height: deviceSize.height * 0.4,
-                    child: Image.asset(
-                      widget.hero.heroImage,
-                      fit: BoxFit.cover,
-                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                ],
+              ),
+              AnimatedPositioned(
+                duration: const Duration(milliseconds: 400),
+                top: animate ? 0 : -300,
+                curve: Curves.fastEaseInToSlowEaseOut,
+                child: SizedBox(
+                  width: deviceSize.width,
+                  height: deviceSize.height * 0.4,
+                  child: Image.asset(
+                    widget.hero.heroImage,
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Positioned.fill(
-            child: SizedBox.expand(
-              child: DraggableScrollableSheet(
-                minChildSize: 0.25,
-                maxChildSize: 0.7,
-                initialChildSize: 0.25,
-                builder:
-                    (BuildContext context, ScrollController scrollController) {
-                  return Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                      ),
+          // ),
+          customDraggableSheet()
+          // Positioned.fill(
+          //   child: SizedBox.expand(
+          //     child: DraggableScrollableSheet(
+          //       minChildSize: 0.25,
+          //       maxChildSize: 0.7,
+          //       initialChildSize: 0.25,
+          //       builder:
+          //           (BuildContext context, ScrollController scrollController) {
+          //         return Container(
+          //           decoration: const BoxDecoration(
+          //             color: Colors.white,
+          //             borderRadius: BorderRadius.only(
+          //               topLeft: Radius.circular(12),
+          //               topRight: Radius.circular(12),
+          //             ),
+          //           ),
+          //           child: SingleChildScrollView(
+          //             controller: scrollController,
+          //             // physics: NeverScrollableScrollPhysics(),
+          //             child: Column(
+          //               crossAxisAlignment: CrossAxisAlignment.start,
+          //               children: [
+          //                 const Padding(
+          //                   padding: EdgeInsets.only(left: 14.0, top: 8),
+          //                   child: Text(
+          //                     "Latest News",
+          //                     style: TextStyle(
+          //                         fontSize: 18,
+          //                         fontWeight: FontWeight.bold,
+          //                         color: Colors.black),
+          //                   ),
+          //                 ),
+          //                 SizedBox(
+          //                   height: 150,
+          //                   child: ListView.builder(
+          //                       scrollDirection: Axis.horizontal,
+          //                       itemCount: widget.hero.heroLatestNews.length,
+          //                       itemBuilder: (context, index) {
+          //                         return Container(
+          //                           width: 250,
+          //                           margin: const EdgeInsets.all(8),
+          //                           decoration: BoxDecoration(
+          //                             borderRadius: BorderRadius.circular(12),
+          //                             image: DecorationImage(
+          //                               image: NetworkImage(
+          //                                 widget.hero.heroLatestNews[index],
+          //                               ),
+          //                               fit: BoxFit.cover,
+          //                             ),
+          //                           ),
+          //                         );
+          //                       }),
+          //                 ),
+          //                 const Padding(
+          //                   padding: EdgeInsets.only(left: 14.0, top: 8),
+          //                   child: Text(
+          //                     "Related Movies",
+          //                     style: TextStyle(
+          //                         fontSize: 18,
+          //                         fontWeight: FontWeight.bold,
+          //                         color: Colors.black),
+          //                   ),
+          //                 ),
+          //                 SizedBox(
+          //                   height: 250,
+          //                   child: ListView.builder(
+          //                       scrollDirection: Axis.horizontal,
+          //                       itemCount: widget.hero.heroLatestNews.length,
+          //                       itemBuilder: (context, index) {
+          //                         return Container(
+          //                           width: 150,
+          //                           margin: const EdgeInsets.all(8),
+          //                           decoration: BoxDecoration(
+          //                             borderRadius: BorderRadius.circular(12),
+          //                             image: DecorationImage(
+          //                               image: NetworkImage(
+          //                                 widget.hero.relatedMovies[index],
+          //                               ),
+          //                               fit: BoxFit.cover,
+          //                             ),
+          //                           ),
+          //                         );
+          //                       }),
+          //                 )
+          //               ],
+          //             ),
+          //           ),
+          //         );
+          //       },
+          //     ),
+          //   ),
+          // ),
+        ],
+      ),
+    );
+  }
+
+  Widget customDraggableSheet() {
+    final hight = MediaQuery.sizeOf(context).height;
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: AnimatedContainer(
+        height: reverseScroll ? hight * 0.60 : hight * 0.24,
+        curve: Curves.easeInOutBack,
+        duration: const Duration(milliseconds: 500),
+        child: Material(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
+          ),
+          child: NotificationListener<UserScrollNotification>(
+            onNotification: (notification) {
+              final ScrollDirection direction = notification.direction;
+              SchedulerBinding.instance.addPostFrameCallback((_) {
+                //
+                if (mounted) {
+                  setState(() {
+                    if (direction == ScrollDirection.reverse &&
+                        notification.metrics.axis == Axis.vertical) {
+                      reverseScroll = true;
+                    } else if (direction == ScrollDirection.forward &&
+                        notification.metrics.axis == Axis.vertical) {
+                      reverseScroll = false;
+                    }
+                    print(reverseScroll);
+                  });
+                }
+              });
+
+              return true;
+            },
+            child: SingleChildScrollView(
+              // controller: scrollController,
+              // physics: NeverScrollableScrollPhysics(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 14.0, top: 14),
+                    child: Text(
+                      "Latest News",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
                     ),
-                    child: SingleChildScrollView(
-                      controller: scrollController,
-                      // physics: NeverScrollableScrollPhysics(),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.only(left: 14.0, top: 8),
-                            child: Text(
-                              "Latest News",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
+                  ),
+                  SizedBox(
+                    height: hight * 0.20,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: widget.hero.heroLatestNews.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            width: 250,
+                            margin: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  widget.hero.heroLatestNews[index],
+                                ),
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 150,
-                            child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: widget.hero.heroLatestNews.length,
-                                itemBuilder: (context, index) {
-                                  return Container(
-                                    width: 250,
-                                    margin: const EdgeInsets.all(8),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(12),
-                                      image: DecorationImage(
-                                        image: NetworkImage(
-                                          widget.hero.heroLatestNews[index],
-                                        ),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  );
-                                }),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(left: 14.0, top: 8),
-                            child: Text(
-                              "Related Movies",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 250,
-                            child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: widget.hero.heroLatestNews.length,
-                                itemBuilder: (context, index) {
-                                  return Container(
-                                    width: 150,
-                                    margin: const EdgeInsets.all(8),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(12),
-                                      image: DecorationImage(
-                                        image: NetworkImage(
-                                          widget.hero.relatedMovies[index],
-                                        ),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  );
-                                }),
-                          )
-                        ],
-                      ),
+                          );
+                        }),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 14.0, top: 8),
+                    child: Text(
+                      "Related Movies",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
                     ),
-                  );
-                },
+                  ),
+                  SizedBox(
+                    height: hight * 0.30,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: widget.hero.heroLatestNews.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            width: 150,
+                            margin: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  widget.hero.relatedMovies[index],
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                  Container(
+                    height: hight * 0.02,
+                  )
+                ],
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
